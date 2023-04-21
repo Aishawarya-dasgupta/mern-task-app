4d826a50d6e8c23115918439fb48abf1c84722d3
Aishawarya Dasgupta <aishawarya.dasgupta.fiem.cse19@teamfuture.in>
2023-04-21 20:20:27 +0530

    connecting to mongodb
    we did this because initially the server was starting first and then the mongodb was getting connected later on which might result to sum error. In the startServer function we are using await and async so that unless the db is getting connected, the server will not listen.
    As initially there was no await or aysnc due to will the server was getting started earlier then mongodb because mongodb needs some time to connect and in that time the server used to start

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const dotevn = require('dotenv').config();
 const express = require('express')
+const connectDB = require('./config/connectDB')
 const app = express()
 
 const PORT = process.env.PORT || 3000;
@@ -11,7 +13,17 @@ app.post('/tasks',(req,res)=>{
     res.send("<h2>This is the tasks post url</h2>")
 })
 
-app.listen(PORT,(req,res) =>{
-    console.log(`server is listening on port ${PORT}`);
-})
 
+
+const startServer = async() => {
+    try{
+        await connectDB()
+        app.listen(PORT,(req,res) =>{
+            console.log(`server is listening on port ${PORT}`);
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+
+startServer();
