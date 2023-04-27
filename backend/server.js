3c8859a5bb9f87a997881c3f1343b2e67c2498f4
Aishawarya Dasgupta <aishawarya.dasgupta.fiem.cse19@teamfuture.in>
2023-04-27 23:08:56 +0530

    making different folder for routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const dotevn = require('dotenv').config();
 const express = require('express')
-const connectDB = require('./config/connectDB')
+const connectDB = require('./config/connectDB') 
 const Task = require('./model/taskModel')
+const taskRoute = require('./routes/taskRoute')
 
 
 const app = express()
@@ -10,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.json())
-app.use(express.urlencoded({ extended:false}))
+app.use(express.urlencoded({ extended:false})) 
+app.use(taskRoute)
 // const logger = async (req,res,next) => {
 //     console.log("Middleware ran")
 //     console.log(req.method)
@@ -21,33 +23,6 @@ app.use(express.urlencoded({ extended:false}))
 app.get('/',(req,res)=>{
     res.send("Home Page")
 })
-
-//Create a task
-app.post('/api/tasks', async(req,res)=>{
-    //console.log(req.body)
-    //res.send("task created")
-    try{
-        const task  = await Task.create(req.body)
-        res.status(201).json(task)
-    }catch(err){
-        res.status(500).json({"error":err})
-    }
-})
-
-//GET/Read Data
-app.get('/api/tasks', async(req,res)=>{
-    try {
-        const tasks = await Task.find()
-        res.status(200).json(tasks)
-    } catch (err) {
-        res.status(500).json({"error":err})
-    }
-})
-
-
-
-
-
 
 const startServer = async() => {
     try{
